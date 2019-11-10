c21376c326d80423830ce4c50141f00762ded297
Dylan Treisman <dylanzt@gmail.com>
2019-11-10 17:55:11 +0100

    Add React, Babel and Webpack

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ComingSoon from "./components/ComingSoon.jsx";
+class App extends Component {
+  render() {
+    return (
+      <ComingSoon />
+    );
+  }
+}
+export default App;
---
+++ src/components/ComingSoon.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ComingSoon = () => (
+  <div>
+    <h1>Coming Soon...</h1>
+    <p>WIP</p>
+  </div>
+);
+export default ComingSoon;
