fecc93c2c1a9ac83258140a8e4c379690e9dd96d
Dylan Treisman <dylanzt@gmail.com>
2019-11-11 14:50:25 +0100

    Add CSS modules

--- src/components/ComingSoon/ComingSoon.jsx
+++ src/components/ComingSoon/ComingSoon.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import styles from "./ComingSoon.css"
+
+console.log(styles);
+
 const ComingSoon = () => (
   <div>
     <h1>Coming Soon...</h1>
-    <p>WIP</p>
+    <p className={styles.text}>WIP</p>
   </div>
 );
+
 export default ComingSoon;
