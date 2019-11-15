2ed7ea1b40edb8ec3ffd001c970ecacaf0e73d4f
Dylan Treisman <dylanzt@gmail.com>
2019-11-15 14:35:40 +0100

    Change root id to app

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 
 import App from "./App.jsx";
 
-const root = document.getElementById("root");
-root ? ReactDOM.render(<App />, root) : false;
+const app = document.getElementById("app");
+app ? ReactDOM.render(<App />, app) : false;
