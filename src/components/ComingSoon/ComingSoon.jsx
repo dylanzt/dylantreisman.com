61adacdbd59504199da9f201621f45357ec6792a
Dylan Treisman <dylanzt@gmail.com>
2019-11-17 10:37:48 +0100

    Update main page copy

--- src/components/ComingSoon/ComingSoon.jsx
+++ src/components/ComingSoon/ComingSoon.jsx
@@ -6,7 +6,7 @@ import Lipsum from "./Lipsum.js";
 const ComingSoon = () => (
   <div>
     <h1>Coming Soon...</h1>
-    <p className={styles.text}>WIP</p>
+    <p className={styles.text}>Welcome to my personal website. It's currently under active development. You can view the source <a href="https://github.com/dylanzt/dylantreisman.com" target="_blank" rel="noopener noreferrer">here</a>.</p>
     {featureFlag.lipsum && (<Lipsum className={styles.text} />)}
   </div>
 );
