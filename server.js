ce1781f6681f98f4a5444e03a14926f83eebec7e
Dylan Treisman <dylanzt@gmail.com>
2020-05-30 23:21:24 +1000

    Add server logging with pino

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const pino = require("pino");
+const expressPino = require("express-pino-logger");
 const path = require("path");
+
+const logger = pino({ level: process.env.LOG_LEVEL || "info" });
+const expressLogger = expressPino({ logger });
 
 const app = express();
 
 const dist = path.join(__dirname, "dist");
+
+app.use(expressLogger);
 
 app.use(express.static(dist));
 
@@ -11,4 +18,6 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(dist, "index.html"));
 });
 
-app.listen(5000);
+app.listen(5000, () => {
+    logger.info("Server running on port 5000");
+});
