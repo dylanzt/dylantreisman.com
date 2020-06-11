1562414bc87cdb8b93f59a6ea62b0ae7c5cb3b0e
Dylan Treisman <dylanzt@gmail.com>
2020-06-11 17:08:55 +1000

    Add close icon for hamburger

--- src/components/Hamburger/index.jsx
+++ src/components/Hamburger/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import classNames from "classnames/bind";
 
 import styles from "./styles";
 
+const cx = classNames.bind(styles);
+
 const Hamburger = () => (
-    <button className={styles.hamburger}>
+    <button className={cx({
+        hamburger: true,
+        close: true,
+    })}>
         <span className={styles.hamburgerText}>
             <span className={styles.screenReaderText}>Open Menu</span>
         </span>
