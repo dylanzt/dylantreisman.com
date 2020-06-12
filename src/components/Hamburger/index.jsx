bdbbf49541eb9cf5fb228ac460b63ed1a54f2e5d
Dylan Treisman <dylanzt@gmail.com>
2020-06-12 11:31:47 +1000

    Add state management to hamburger

--- src/components/Hamburger/index.jsx
+++ src/components/Hamburger/index.jsx
@@ -4,16 +4,36 @@ import classNames from "classnames/bind";
 import styles from "./styles";
 
 const cx = classNames.bind(styles);
+class Hamburger extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            isTrayOpen: false,
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-const Hamburger = () => (
-    <button className={cx({
-        hamburger: true,
-        close: true,
-    })}>
-        <span className={styles.hamburgerText}>
-            <span className={styles.screenReaderText}>Open Menu</span>
-        </span>
-    </button>
-);
+    handleClick() {
+        this.setState(state => ({
+            isTrayOpen: !state.isTrayOpen,
+        }));
+    }
+
+    render() {
+        return (
+            <button
+                onClick={this.handleClick}
+                className={cx({
+                    hamburger: true,
+                    close: this.state.isTrayOpen,
+                })}
+            >
+                <span className={styles.hamburgerText}>
+                    <span className={styles.screenReaderText}>Open Menu</span>
+                </span>
+            </button>
+        );
+    }
+}
 
 export default Hamburger;
